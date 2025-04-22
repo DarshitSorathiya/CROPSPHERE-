d7cae67ebd6a08cb4c74a785d745288b09fdf2dd
darshitsorathiya <darshitsorthiya@gmail.com>
2025-04-22 12:35:41 +0530

    backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 7000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("ERROR :" + err);
+    });
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,105 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    index: true,
+    trim: true,
+    validate: [validator.isEmail, "Email format is invalid"],
+  },
+  phoneNo: {
+    type: String,
+    required: true,
+    trim: true,
+    match: [/^[0-9]{10}$/, "Phone number must be exactly 10 digits"],
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+  },
+  dob: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value <= new Date();
+      },
+      message: "Date pf birth cannot be in future",
+    },
+  },
+  refreshToken: {
+    type: String,
+  },
+  age: { type: Number, default: 0 },
+});
+
+userSchema.pre("save", async function (next) {
+  const today = new Date();
+
+  this.age = today.getFullYear() - this.dob.getFullYear();
+
+  if (
+    today.getMonth() < this.dob.getMonth() ||
+    (today.getMonth() == this.dob.getMonth() &&
+      today.getDay() < this.dob.getDay())
+  ) {
+    this.age--;
+  }
+
+  if (!this.isModified("password")) return next;
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.method.isPasswordCorrect = async function (enteredPassword) {
+  if (!this.password) {
+    throw new Error("Password field is missing");
+  }
+  return bcrypt.compare(this.password, enteredPassword);
+};
+
+userSchema.method.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.method.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
